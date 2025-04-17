b2c0a97948988891e1cbbf064b2a0783e68744a9
Chris <Chris.nathaniel29@gmail.com>
2025-04-17 17:00:44 +0700

    m

--- crypto/models.py
+++ crypto/models.py
@@ -4,7 +4,7 @@ from django import forms
 # Create your models here.
 class Wallet(models.Model):
     session_id = models.CharField(max_length=255, unique=True)
-    token = models.CharField(max_length=64, unique=True)
+    token = models.CharField(max_length=64)
     balance = models.DecimalField(max_digits=60, decimal_places=0)
     quantity = models.DecimalField(max_digits=60, decimal_places=3)
     average_price = models.DecimalField(max_digits=25, decimal_places=2, default=0.00)
